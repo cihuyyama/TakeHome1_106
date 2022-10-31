9e3cbecdca4648297c57da1fe3015c0db6c95ba5
cihuyyama <miqbalalhabib@gmail.com>
2022-10-31 15:53:11 +0700

    nambahin file html dan controller

---
+++ src/main/java/ws/a/TakeHome1/myController.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ws.a.TakeHome1;
+
+/**
+ *
+ * @author iqbal al habib
+ */
+public class myController {
+    
+}
